b10233d6f06443063cb4b64839c80d67419aec2c
Meik0Yuu <dussot-a@3il.fr>
2026-05-19 17:06:55 +0200

    ajout défense

--- src/state/Etat.java
+++ src/state/Etat.java
@@ -6,4 +6,5 @@ public interface Etat {
 	String getNomEtat();
     void appliquerEffet(Joueur joueur);
     int modifierAttaque(Joueur joueur, int base);
+    int modifierDefense(Joueur joueur, int base);
 }
